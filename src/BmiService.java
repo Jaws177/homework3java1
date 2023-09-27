dc29e931c74dc30c01d9b6ae27bfe27bc856193c
tester <alekseyOT325@yandex.ru>
2023-09-27 23:25:57 +0300

    correct2

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,9 +1,9 @@
 public class BmiService {
-    public int calculate(int a, int b) {
+    public int calculate(int height, int weight) {
         double converter = 100; //переменная для перевода сантиметров в метры
-        double metres = a / converter; //переводим сантиметры в метры
+        double metres = height / converter; //переводим сантиметры в метры
         double square = Math.pow(metres, 2); //возводим метры в квадрат
-        double index = b / square; //вычисляем индекс
+        double index = weight / square; //вычисляем индекс
         return (int) index; //возвращаем значение, убрав знаки после запятой
     }
 }
